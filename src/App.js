e398c8c7f93629aa5cc22d5906ae1dad7528ffd1
shenny85 <143647445+shenny85@users.noreply.github.com>
2023-11-27 19:00:46 -0500

    API's and useEffect useContext

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import {useState} from 'react';
+import {useEffect, useContext} from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import BooksContext from './Context/books';
 
 function App() {
-    const [books, setBooks] = useState([]);
-    const editBookById = (id, newTitle) => {
-        const updatedBooks = books.map((book) => {
-            if (book.id  === id) {
-                return { ...book, title: newTitle };
-            }
-            return book;
-        });
+    const {fetchBooks} = useContext(BooksContext);
 
-        setBooks(updatedBooks);
-    }
-    const deleteBookById= (id) => {
-        const updatedBooks = books.filter((book) => {
-            return book.id !== id;
-        });
-        setBooks(updatedBooks)
-    }
-    
-    const createBook = (title) => {
-       const updatedBooks = [...books,{ id: Math.round(Math.random()*9999), title }];
-       setBooks(updatedBooks);
-    };
 
-    return <div className="app">
+    useEffect(() => {
+        fetchBooks(); }, []);
+
+
+    //DON'T DO THIS: do not directly call fetchBooks() here because it will cause an infinite loop of requests
+
+    return (
+    <div className="app">
         <h1>Reading List</h1>
-        <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
-        <BookCreate onCreate={createBook} />
-        </div>;
+        <BookList />
+        <BookCreate />
+        </div>);
 }
 
 export default App;
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,18 +1,20 @@
 import {useState} from 'react';
 import BookEdit from './BookEdit';
+import useBooksContext from '../hooks/use-books-context';
 
-function BookShow({book, onDelete, onEdit}) {
+function BookShow({book}) {
     const [showEdit, setShowEdit] = useState(false);
-const handleDeleteClick = () => {
-    onDelete(book.id)
+    const {deleteBookById} = useBooksContext();
+
+    const handleDeleteClick = () => {
+        deleteBookById(book.id);
 }
-const handleEditClick = () => {
-    setShowEdit(!showEdit);
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
 };
 
-const handleSubmit = (id, newTitle) => {
-    setShowEdit(false);
-    onEdit(id, newTitle)
+    const handleSubmit = () => {
+        setShowEdit(false);
 };
 
 let content = <h3>{book.title}</h3>;
